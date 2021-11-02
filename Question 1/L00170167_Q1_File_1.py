5ae576e35c91b41396e23dc0e36a0c5e4d9e422c
derektroy <L00170167@student.lyit.ie>
2021-11-02 21:28:13 +0000

    Update L00170167_Q1_File_1.py

--- Question 1/L00170167_Q1_File_1.py
+++ Question 1/L00170167_Q1_File_1.py
@@ -9,3 +9,25 @@
 # Description   :   Question 1 - Create an SSH Connection to a VM
 #
 """
+# import paramiko module
+import paramiko
+
+# configure the login details
+hostname = "192.168.1.60"
+username = "test"
+password = "letterkenny"
+
+# initialize the SSH client
+client = paramiko.SSHClient()
+
+# add to known hosts
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# connect to the server, if connected print, if fails print other statement
+try:
+    client.connect(hostname=hostname, username=username, password=password)
+    print("Connected to: ", hostname)
+
+except:
+    print("[!] Cannot connect to", hostname)
+    exit()
