ea97adce9369a79e33e420484208819ea6e73466
derektroy <L00170167@student.lyit.ie>
2021-11-02 20:50:32 +0000

    Started Question 1

---
+++ Question 1/L00170167_Q1_File_1.py
@@ -0,0 +1,11 @@
+"""
+#
+# File          :   L00170167_Q1_File_1.py
+# Created       :    
+# Author        :   Derek Troy
+# Version       :   1.0.0
+# Licensing     :   (C) 2021 Derek Troy LYIT
+#                   Available under GNU Public License (GPL)
+# Description   :   Question 1 - Create an SSH Connection to a VM
+#
+"""
