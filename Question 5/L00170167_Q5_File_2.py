5e8381a8a555f910d324d201504f0de7a647aa7c
derektroy <L00170167@student.lyit.ie>
2021-11-06 17:17:23 +0000

    Lost Time

---
+++ Question 5/L00170167_Q5_File_2.py
@@ -0,0 +1,37 @@
+"""
+#
+# File          :   L00170167_Q5_File_2.py
+# Created       :    
+# Author        :   Derek Troy
+# Version       :   1.0.0
+# Licensing     :   (C) 2021 Derek Troy LYIT
+#                   Available under GNU Public License (GPL)
+# Description   :   Connect to Remote Host, Create Dirs and Install Curl
+#
+"""
+# import paramiko module
+import paramiko
+import time
+
+# configure the login details
+hostname = "192.168.1.60"
+username = "test"
+password = "letterkenny"
+
+# initialize the SSH client
+client = paramiko.SSHClient()
+
+# add to known hosts
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# connect to the server, if connected print, if fails print other statement
+try:
+    client.connect(hostname=hostname, username=username, password=password)
+    connection = client.invoke_shell()
+    client.exec_command("mkdir Labs\n")
+    client.exec_command("mkdir Labs/lab1\n")
+    client.exec_command("mkdir Labs/lab2\n")
+    client.exec_command("echo letterkenny | sudo -S apt install -y curl\n")
+except:
+    print("[!] Cannot connect to", hostname)
+    exit()
