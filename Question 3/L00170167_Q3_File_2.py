d9c6a10cc5526ce6afab1ec9d946f53717431595
derektroy <L00170167@student.lyit.ie>
2021-11-11 20:08:02 +0000

    Questions Changed - ReOrg

--- Question 3/L00170167_Q3_File_2.py
+++ Question 3/L00170167_Q3_File_2.py
@@ -1,24 +1,33 @@
 """
 #
-# File          :   L00170167_Q3_File_2.py
+# File          :   L00170167_Q1_File_1.py
 # Created       :    
 # Author        :   Derek Troy
 # Version       :   1.0.0
 # Licensing     :   (C) 2021 Derek Troy LYIT
 #                   Available under GNU Public License (GPL)
-# Description   :   File to Scrape and Parse a Webpage from Apache Server
+# Description   :   Question 1 - Create an SSH Connection to a VM
 #
 """
-# Import Required Modules
-import requests
-from bs4 import BeautifulSoup
+# import paramiko module
+import paramiko
 
-# What Website are we going to look at
-Web_url = "http://192.168.1.60"
+# configure the login details
+hostname = "192.168.1.60"
+username = "test"
+password = "letterkenny"
 
-# What are we going to do with that URL
-r = requests.get(Web_url)
+# initialize the SSH client
+client = paramiko.SSHClient()
 
-# Parse HTML Code
-soup = BeautifulSoup(r.content, 'lxml')
-print(soup.prettify())
+# add to known hosts
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# connect to the server, if connected print, if fails print other statement
+try:
+    client.connect(hostname=hostname, username=username, password=password)
+    print("Connected to: ", hostname)
+
+except:
+    print("[!] Cannot connect to", hostname)
+    exit()
