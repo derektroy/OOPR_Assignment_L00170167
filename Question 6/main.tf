5640702d6fa93dbea1ca58a90ec3be9b7bebfa15
derektroy <L00170167@student.lyit.ie>
2021-11-23 14:43:09 +0000

    First Terraform Appy

--- Question 6/main.tf
+++ Question 6/main.tf
@@ -1,20 +1,20 @@
 terraform {
-required_providers {
-aws = {
-source = "hashicorp/aws"
-version = "~> 3.27"
-}
-}
-required_version = ">= 0.14.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
 }
 provider "aws" {
-profile = "default"
-region = "us-west-2"
+  profile = "default"
+  region  = "us-west-2"
 }
 resource "aws_instance" "app_server" {
-ami = "ami-830c94e3"
-instance_type = "t2.micro"
-tags = {
-Name = "LYITExampleAppServerInstance"
-}
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "LYITExampleAppServerInstance"
+  }
 }
